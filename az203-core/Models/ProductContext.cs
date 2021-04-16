c3acbbca5a46ed4af70c95817628480dff0a2c4a
Alexander Hernández Herrera <aherrera@itcoint.com>
2021-04-16 16:15:26 -0600

    Agregar archivos de proyecto.

---
+++ az203-core/Models/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace az203_core.Models
+{
+    public class ProductContext : DbContext
+    {
+        // Replace the database connection string  here
+        readonly string connectionstring = "Server=tcp:demovm.database.windows.net,1433;Initial Catalog=appregdemo;Persist Security Info=False;User ID=demouser;Password=Demo123456789-;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionstring);
+            base.OnConfiguring(options);
+        }
+
+    }
+}
